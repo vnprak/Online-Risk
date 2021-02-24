80a40eb29a259615c15458b8379fc54617945577
vnprak <vp1148@nyu.edu>
2021-02-24 11:30:12 +0000

    Added stub software library, see #2

---
+++ risk.py
@@ -0,0 +1,4 @@
+def info():  
+    '''Prints a basic library description'''
+    print("Software library for the Online Risk project.")
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='risk',
+      version='1.0',
+      description='Risk Stub Library',
+      author='vnprak',
+      author_email='vp1148@nyu.edu',
+      url='https://github.com/vnprak/Online-Risk/',
+      py_modules=['risk'],
+     )
+
