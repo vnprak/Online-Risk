1486fcc73fba5e19427c2a0fbf22df76cc8c57e5
vnprak <vp1148@nyu.edu>
2021-03-03 04:13:03 -0500

    created stub functions

--- risk.py
+++ risk.py
@@ -5,3 +5,28 @@ def info():
 def detect_tag():
     print("No tag detected")
     return False
+
+def read_tag():
+    print("Error reading tag info")
+    return False
+    
+def display_on():
+    print("Display turned ON")
+    
+def display_off():
+    print("Display turned OFF")
+    
+def clear_display():
+    print("Display cleared")
+    
+def text_to_display(text)
+    print(text)
+    
+def i2c_send_output(addr,out)
+    print(out + " sent to " + addr)
+    
+def i2c_read_input(addr)
+    print("reading from " + addr)
+    
+def buzz(dur)
+    print("Buzzer buzzing for " + dur + seconds)
